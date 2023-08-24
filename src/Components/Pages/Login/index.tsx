ffb2e4a6cca03a5ca8d51262cb357e7150036b8f
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-24 18:10:12 -0300

    teste

--- src/Components/Pages/Login/index.tsx
+++ src/Components/Pages/Login/index.tsx
@@ -54,7 +54,7 @@ export const Login = () => {
         <>
           <div className="container-logo">
             <img
-              src={'./assets/petluxlogo.png'}
+              src={'../assets/petluxlogo.png'}
               alt="logo-petlux"
               className="logo"
             />
