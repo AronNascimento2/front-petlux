243eb4fc1a58a907e84e1be35d6ab8386170f184
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-24 16:39:56 -0300

    fixname

--- src/Components/Chat/index.tsx
+++ src/Components/Chat/index.tsx
@@ -43,7 +43,7 @@ export const Chat = ({ className }: props) => {
               {' '}
               <div className="infochat">
                 <Avatar className="avatar" />
-                <p>Aron Nasciment</p>
+                <p>Aron Nascimento</p>
               </div>
               <div className="icon-close">
                 <CloseIcon onClick={handleCloseModal} />
