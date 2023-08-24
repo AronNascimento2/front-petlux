a6db74d41d331a9807da7a16b8599615e87fe196
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-24 16:42:25 -0300

    fixpassword

--- src/constants/Users/index.ts
+++ src/constants/Users/index.ts
@@ -1,13 +1,13 @@
 export const Users = [
-    {
-        name: 'Aron',
-        email:'aron@teste.com',
-        senha: '@Ar12345679'
-    },
+  {
+    name: 'Aron',
+    email: 'aron@teste.com',
+    senha: '@Ar12345678'
+  },
 
-    {
-        name: 'Indira',
-        email:'indira@teste.com',
-        senha: '@Id12345679'
-    }
+  {
+    name: 'Indira',
+    email: 'indira@teste.com',
+    senha: '@Id12345678'
+  }
 ]
