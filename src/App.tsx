62d0ca194034aa4e54c95e8010a1139fcb66499c
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-24 11:52:25 -0300

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,20 @@ import { ThemeProvider } from 'styled-components'
 import theme from './constants/themeDefault'
 import 'react-toastify/dist/ReactToastify.css'
 import AppRouter from './Routes/AppRouter'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Login } from './Components/Pages/Login'
+import { Home } from './Components/Pages/Home'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Toasts />
+      <BrowserRouter basename="/front-petlux">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>{' '}
       <AppRouter />
       <ToastContainer theme="colored" autoClose={1000} limit={1} />
     </ThemeProvider>
