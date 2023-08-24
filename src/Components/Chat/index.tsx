9bf8d264e8cea7d61850c7c95d268328f261b3b5
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-24 17:18:02 -0300

    fix

--- src/Components/Chat/index.tsx
+++ src/Components/Chat/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState } from 'react'
 import * as S from './styles'
 import MessageIcon from '@mui/icons-material/Message'
 import Avatar from '@mui/material/Avatar'
@@ -8,9 +8,9 @@ import { ComposeMessage } from '../ComposeMessage'
 
 type props = { className: string }
 
+// ...
 export const Chat = ({ className }: props) => {
   const [show, setShow] = useState(false)
-  const containerRef = useRef(null)
 
   const handleOpenModal = () => {
     setShow(true)
@@ -20,25 +20,19 @@ export const Chat = ({ className }: props) => {
     setShow(false)
   }
 
-  useEffect(() => {
-    if (show) {
-      document.addEventListener('mousedown', handleCloseModal)
-    } else {
-      document.removeEventListener('mousedown', handleCloseModal)
-    }
-    return () => {
-      document.removeEventListener('mousedown', handleCloseModal)
-    }
-  }, [show])
+  const handleModalClick = (event) => {
+    // Evitar a propagação do evento para o elemento pai
+    event.stopPropagation()
+  }
 
   return (
-    <div ref={containerRef}>
+    <div>
       <S.Container onClick={handleOpenModal} className={className}>
         <MessageIcon color="info" className="icon" />
       </S.Container>
       {show && (
         <S.ChatWrapper>
-          <S.ModalChat>
+          <S.ModalChat onClick={() => handleModalClick(show)}>
             <div className="headerchat">
               {' '}
               <div className="infochat">
