f5e33123ef1a816470f558771800e9786e0f08df
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-24 11:31:53 -0300

    teste

--- src/Routes/routesPath.tsx
+++ src/Routes/routesPath.tsx
@@ -1,27 +1,27 @@
-import { CreateNewAccount } from "../Components/Pages/CreateNewAccount";
-import { Home } from "../Components/Pages/Home";
-import { Login } from "../Components/Pages/Login";
-import { RecoveryPassword } from "../Components/Pages/RecoveryPassword";
+import { CreateNewAccount } from '../Components/Pages/CreateNewAccount'
+import { Home } from '../Components/Pages/Home'
+import { Login } from '../Components/Pages/Login'
+import { RecoveryPassword } from '../Components/Pages/RecoveryPassword'
 
 export const ROUTES_PATH = [
   {
-    path: "",
+    path: 'login',
     element: <Login />,
-    isPrivate: false, // Public route
+    isPrivate: false // Public route
   },
   {
-    path: "home",
-    element: <Home />,
-    isPrivate: true, // Private route
+    path: 'home',
+    element: <Home />
+    // isPrivate: true, // Private route
   },
   {
-    path: "recoverypassword",
-    element: <RecoveryPassword />,
-    isPrivate: false, // Public route
+    path: 'recoverypassword',
+    element: <RecoveryPassword />
+    // isPrivate: false, // Public route
   },
   {
-    path: "createnewaccount",
-    element: <CreateNewAccount />,
-    isPrivate: false, // Public route
-  },
-];
+    path: 'createnewaccount',
+    element: <CreateNewAccount />
+    // isPrivate: false, // Public route
+  }
+]
