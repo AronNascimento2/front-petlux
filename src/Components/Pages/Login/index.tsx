417263e475c5bf551ab6f6dc8c5782565678f61c
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-24 18:33:28 -0300

    teste

--- src/Components/Pages/Login/index.tsx
+++ src/Components/Pages/Login/index.tsx
@@ -53,7 +53,11 @@ export const Login = () => {
       ) : (
         <>
           <div className="container-logo">
-            <img src={'Petluxlogo.jpeg'} alt="logo-petlux" className="logo" />
+            <img
+              src={'assets/Petluxlogo.jpeg'}
+              alt="logo-petlux"
+              className="logo"
+            />
           </div>
           <div className="container-div">
             <input
