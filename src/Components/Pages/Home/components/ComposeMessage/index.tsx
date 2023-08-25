e8e9142689472cefb7788dc9737d008adaf6790b
AronNascimento2 <120020442+AronNascimento2@users.noreply.github.com>
2023-08-25 14:21:45 -0300

    scroll

--- src/Components/Pages/Home/components/ComposeMessage/index.tsx
+++ src/Components/Pages/Home/components/ComposeMessage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import * as S from './styles'
 
 interface ComposeProps {
@@ -7,8 +7,18 @@ interface ComposeProps {
 }
 
 const ComposeMessage: React.FC<ComposeProps> = ({ messages, isUser }) => {
+  const messageContainerRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    // Mantém o scroll no final quando novas mensagens são adicionadas
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight
+    }
+  }, [messages])
+
   return (
-    <S.Container>
+    <S.Container ref={messageContainerRef}>
       {messages.map((message, index) => (
         <S.BubbleContainer key={index} isUser={isUser}>
           <S.Avatar
